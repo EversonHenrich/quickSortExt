ea375612dff1fd6bc3c3bed3931be3fe8604e2a1
Everson Henrich <eversonhs.es@gmail.com>
2019-10-28 17:35:45 -0300

    Quicksort Externo para tipo genérico de dados

--- main.c
+++ main.c
@@ -1,54 +1,71 @@
-#include "quickSortExt.c"
+#include "quickSortExt.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
+typedef struct {
+    int a;
+}teste;
 
 
+int compara(teste *a, teste *b)
+{
+    if(a->a == b->a)
+        return 0;
+    else if(a->a > b->a)
+        return 1;
+    else
+        return -1;
+}
 
-void gerarArquivoInt(FILE *arq, int nElem)
+
+int gerarArquivoInt(FILE *arq, int nElem)
 {
     int i = 0;
-    int *value = (int*)calloc(nElem, sizeof(int));
+    teste *value = (teste*)calloc(nElem, sizeof(teste));
 
     srand(time(NULL));
     rewind(arq);
 
     for(i = 0; i < nElem; i++)
     {
-        // value[i] = nElem - i;
-        value[i] = rand()%1000;
+        value[i].a = rand()%1000;
     }
-    fwrite(value, sizeof(int), nElem, arq);
+    return fwrite(value, sizeof(teste), nElem, arq);
 }
 
 
 
 int main()
 {
-    FILE *arq = fopen("test1.bin", "rb+");
-    int value = 0;
+    FILE *arq = fopen("test1.bin", "wb+");
+    teste value;
     int li = 0;
     int i = 0;
     int j = 0;
     short int o = 0;
 
-    gerarArquivoInt(arq, 100);
+    printf("%d valores gerados.\n", gerarArquivoInt(arq, 50));
     printf("Arquivo antes do quickSortExterno\n");
-    while(li < 100)
+
+    while(li < 50)
     {
-        lerInf(arq, &value, &li, sizeof(int), &o);
-        printf("%d - \t%d\n", li, value);
+        lerInf(arq, (void*)(&value), &li, sizeof(teste), &o);
+        printf("%d - \t%d\n", li, value.a);
     }
-    quickSortExterno(arq, 0, 99, 20);
+
+    quickSortExterno(arq, 0, 49, 15, sizeof(teste), compara);
+
     li = 0;
     printf("==========================\n");
     printf("Arquivo apos quickSortExterno\n");
-    while(li < 100)
+
+    while(li < 50)
     {
-        lerInf(arq, &value, &li, sizeof(int), &o);
-        printf("%d - \t%d\n", li, value);
+        lerInf(arq, (void*)(&value), &li, sizeof(teste), &o);
+        printf("%d - \t%d\n", li, value.a);
     }
+    
     return 0;
 }
--- quickSortExt.h
+++ quickSortExt.h
@@ -1,85 +1,117 @@
+#ifndef _H_QUICKSORTEXT
+#define _H_QUICKSORTEXT
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
+
+/* Retornos da função de comparação
+   int comp(a,b)
+   a>b    1
+   a==b    0
+   a<b    -1
+*/
 
 /*  void lerSup: 
-        Lê nBytes do arquivo apontado por arqLS na posição lS*nBytes distante do início do arquivo, armazena os dados lidos em reg, decrementa lS e
+        Ação: Lê nBytes do arquivo apontado por arqLS na posição lS*nBytes distante do início do arquivo, armazena os dados lidos em reg, decrementa lS e
 altera ordem para 0.
+        Argumentos:
             - FILE *arqLS - ponteiro para o arquivo onde será realizada a leitura.
-            - int *reg - ponteiro para inteiro da variável que armazenará os dados lidos.
+            - void *reg - ponteiro da variável que armazenará os dados lidos.
             - int *lS - ponteiro para inteiro que indica a posição a ser lida.
+            - int nBytes - variável que indica o tamanho dos registros.
             - short int *ordem - ponteiro para inteiro (short) que indica qual será a próxima leitura(1 para lerSup e 0 para lerInf).
-        Sem retorno.
+        Retorno: Sem retorno.
 */
-void lerSup(FILE *arqLS, int *reg, int *lS, int nBytes, short int *ordem);
+void lerSup(FILE *arqlS, void *reg, int *lS, int nBytes, short int *ordem);
 
 /*  void lerInf: 
-        Lê nBytes do arquivo apontado por arqLI na posição lI*nBytes distante do início do arquivo, armazena os dados lidos em reg, incrementa lI e
+        Ação: Lê nBytes do arquivo apontado por arqLI na posição lI*nBytes distante do início do arquivo, armazena os dados lidos em reg, incrementa lI e
 altera ordem para 1.
+        Argumentos:
             - FILE *arqLI - ponteiro para o arquivo onde será realizada a leitura.
-            - int *reg - ponteiro para inteiro da variável que armazenará os dados lidos.
+            - void *reg - ponteiro da variável que armazenará os dados lidos.
             - int *lI - ponteiro para inteiro que indica a posição a ser lida.
+            - int nBytes - variável que indica o tamanho dos registros.
             - short int *ordem - ponteiro para inteiro (short) que indica qual será a próxima leitura(1 para lerSup e 0 para lerInf).
-        Sem retorno.
+        Retorno: Sem retorno.
 */
-void lerInf(FILE *arqLI, int *reg, int *lI, int nBytes, short int *ordem);
+void lerInf(FILE *arqlI, void *reg, int *lI, int nBytes, short int *ordem);
 
-/*  void escSup: 
-        Escreve nBytes da variável reg no arquivo apontado por arqES na posição eS*nBytes distante do início do arquivo e decrementa eS.
+/*  void escSup:
+        Ação: Escreve nBytes da variável reg no arquivo apontado por arqES na posição eS*nBytes distante do início do arquivo e decrementa eS.
+        Argumentos:
             - FILE *arqES - ponteiro para o arquivo onde será realizada a escrita.
-            - int *reg - ponteiro para inteiro da variável que terá seus dados escritos no arquivo.
-            - int *eS - ponteiro para inteiro que indica a posição a ser escrita
-        Sem retorno.
+            - void *reg - ponteiro da variável que terá seus dados escritos no arquivo.
+            - int *eS - ponteiro para inteiro que indica a posição a ser escrita.
+            - int nBytes - variável que indica o tamanho dos registros.
+        Retorno: Sem retorno.
 */
-void escSup(FILE *arqES, int *reg, int *eS, int nBytes);
+void escSup(FILE *arqES, void *reg, int *eS, int nBytes);
 
 /*  void escInf: 
-        Escreve nBytes da variável reg no arquivo apontado por arqEI na posição eI*nBytes distante do início do arquivo e incrementa eI.
+        Ação: Escreve nBytes da variável reg no arquivo apontado por arqEI na posição eI*nBytes distante do início do arquivo e incrementa eI.
+        Argumentos:
             - FILE *arqEI - ponteiro para o arquivo onde será realizada a escrita.
-            - int *reg - ponteiro para inteiro da variável que terá seus dados escritos no arquivo.
+            - void *reg - ponteiro da variável que terá seus dados escritos no arquivo.
             - int *eI - ponteiro para inteiro que indica a posição a ser escrita.
-        Sem retorno.
+            - int nBytes - variável que indica o tamanho dos registros.
+        Retorno: Sem retorno.
 */
-void escInf(FILE *arqEI, int *reg, int *eI, int nBytes);
+void escInf(FILE *arqEI, void *reg, int *eI, int nBytes);
 
 /*  void inserirArea:
-        Insere a variável reg de forma ordenada na área da memória primária apontada por area e incrementa areaOcupada.
-            - int *reg - ponteiro para inteiro da variável que será armazenada na área.
-            - int *area - ponteiro para inteiro da área da memória primária.
+        Ação: Insere a variável reg de forma ordenada na área da memória primária apontada por area e incrementa areaOcupada.
+        Argumentos:
+            - void *area - ponteiro da área da memória primária.
             - int *areaOcupada - ponteiro para inteiro da variável que armazena a area ocupada da memória primária.
-        Sem retorno.
+            - void *reg - ponteiro da variável que será armazenada na área.
+            - int nBytes - variável que indica o tamanho dos registros.
+            - int(*comp)(void*,void*) - ponteiro para a função de comparação.
+        Retorno: Sem retorno.
 */
-void inserirArea(int *reg, int *area, int *areaOcupada);
+void inserirArea(void *area, int *areaOcupada, void *reg, int nBytes, int(*comp)(void*,void*));
 
 /*  void rmvPrimeiroArea:
-        Remove o primeiro elemento armazenado na área da memória primária apontada por area e decrementa areaOcupada.
-            - int *area - ponteiro para inteiro da área da memória primária.
+        Ação: Remove o primeiro elemento armazenado na área da memória primária apontada por area e decrementa areaOcupada.
+        Argumentos:
+            - void *area - ponteiro da área da memória primária.
             - int *areaOcupada - ponteiro para inteiro da variável que armazena a area ocupada da memória primária.
-        Sem retorno.
+            - int nBytes - variável que indica o tamanho dos registros.
+        Retorno: Sem retorno.
 */
-void rmvPrimeiroArea(int *area, int *areaOcupada);
+void rmvPrimeiroArea(void *area, int *areaOcupada, int nBytes);
 
 /*  void particao:
-        Particiona o arquivo apontado por arq em três áreas:
-        área 1 - Elementos menores que o menor elemento da área 2.
-        área 2 - Elementos ordenados.
-        área 3 - Elementos maiores que o maior elemento da área 2.
-            - FILE *arq - ponteiro para o arquivo à ser particionado.
-            - int inicio - inteiro que indica o primeiro elemento do vetor à ser particionado.
-            - int fim - inteiro que indica o último elemento do vetor à ser particionado.
+        Ação: Particiona o arquivo apontado por arq em três subarquivo:
+        área 1 - Registros com o valor menor ou igual que o registro com o menor valor da área 2.
+        área 2 - Registros ordenados.
+        área 3 - Registros com o valor maior ou igual que o registro com o maior valor da área 2.
+        Argumentos:
+            - FILE *arq - ponteiro para o arquivo a ser particionado.
+            - int inicio - inteiro que indica o primeiro elemento do vetor a ser particionado.
+            - int fim - inteiro que indica o último elemento do vetor a ser particionado.
             - int *i - ponteiro para inteiro que indica o início da área 2.
             - int *j - ponteiro para inteiro que indica o fim da área 2.
             - int tamArea - inteiro que indica o tamanho da área na memória principal.
-        Sem retorno.
+            - int nBytes - variável que indica o tamanho dos registros.
+            - int(*comp)(void*,void*) - ponteiro para a função de comparação.
+        Retorno: Sem retorno.
 */
-void particao(FILE *arq, int inicio, int fim, int *i, int*j, int tamArea);
+void particao(FILE *arq,int inicio, int fim, int *i, int *j, int tamArea, int nBytes, int(*comp)(void*,void*));
 
 /*  void quickSortExterno:
-        Ordena os dados do arquivo apontado por arq.
-            - FILE *arq - ponteiro para o arquivo à ser ordenado.
-            - int inicio - inteiro que indica o primeiro elemento do vetor à ser ordenado.
-            - int fim - inteiro que indica o último elemento do vetor à ser ordenado.
-            - int tamArea - inteiro que indica o tamanho da área na memória principal.
-        Sem retorno.
+        Ação: Ordena os registros do arquivo apontado por arq.
+        Argumentos:
+            - FILE *arq - ponteiro para o arquivo a ser ordenado.
+            - int inicio - inteiro que indica o primeiro registro do arquivo a ser ordenado.
+            - int fim - inteiro que indica o último registro do arquivo a ser ordenado.
+            - int tamArea - inteiro que indica o tamanho da área na memória primária.
+            - int nBytes - variável que indica o tamanho dos registros.
+            - int(*comp)(void*,void*) - ponteiro para a função de comparação.
+        Retorno: Sem retorno.
 */
-void quickSortExterno(FILE *arq, int inicio, int fim, int tamArea);
+void quickSortExterno(FILE *arq, int inicio, int fim, int tamArea, int nBytes, int (comp)(void*,void*));
+
+#endif
